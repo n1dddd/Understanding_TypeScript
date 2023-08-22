e6d88c2e0e3a9dad5330b95087eac18cc390f2c0
Daniel <danielseniv@outlook.com>
2023-08-22 18:19:44 -0400

    Basics of TypeScript

---
+++ course_starting_project/basics.ts
@@ -0,0 +1,75 @@
+/*
+function add(n1, n2) { example function in vanilla js, not breaking
+  return n1 + n2;
+}
+
+const number1 = 5;
+const number2 = 2.8;
+
+const result = add(number1, number2);
+console.log(result);
+
+
+function add(n1, n2) {
+  return n1 + n2;
+}
+
+const number1 = "5";
+const number2 = 2.8;
+
+const result = add(number1, number2);
+console.log(result);
+
+breaks the result, JavaScript recieves a string,
+so converts number2 to a string as well, and 
+concatenates and returns string (because it recieved 1 string);
+
+
+function add(n1: number, n2: number) {
+  //adding type assignments ensures that function returns and expected value
+  if (typeof n1 !== "number" && typeof n2 !== "number") {
+    throw new Error("Incorrect Input");
+  }
+  return n1 + n2;
+}
+
+const number1 = "5"; //would not compile if 5 was a string
+const number2 = 2.8;
+
+const result = add(number1, number2);
+console.log(result);
+
+Down side f using a forced validation is that you can only 
+  find the bug during runtime--typescript will throw a 
+  compilation warning
+
+  JavaScript is dynamically typed, meaning you can set a 
+  varialbe to a number initially, but later on change 
+  that variable to a string and be perfectly fine --
+  why there is a typeof operator if code depends on a certain
+  type.
+
+  TypeScript is statically typed, variables are declared 
+  hands on during development. This is to ensure consistency,
+  and making sure that you are clear in what type of type data
+  should be.
+*/
+
+function add(n1: number, n2: number, showResult: boolean, phrase: string) {
+  //this syntax above is used by the TypeScript compiler
+  const result = n1 + n2;
+  if (showResult) {
+    console.log(phrase + result);
+  } else {
+    return result;
+  }
+}
+
+const number1 = 5; //because TypeScript has type inference, no need to declare number1: number = 5
+const number2 = 2.8;
+const printResult = true;
+const resultPhrase = "Result is";
+
+//Core concept of TypeScript is to check types and yell at us if we are setting types incorrectly
+
+add(number1, number2, printResult, resultPhrase);
