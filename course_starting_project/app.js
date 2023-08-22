4116b64575d5a51b3aa69db844ee74895c8cdebf
Daniel <danielseniv@outlook.com>
2023-08-22 18:19:07 -0400

    Start fresh to work with arrays and objects in TypeScript

--- course_starting_project/app.js
+++ course_starting_project/app.js
@@ -1,74 +1,15 @@
-/*
-function add(n1, n2) { example function in vanilla js, not breaking
-  return n1 + n2;
+var person = {
+    //{} TypeScript notation for object type
+    //hover over person and see that the object key value pairs have ';' at the end
+    name: "Maximilian",
+    age: 30,
+    hobbies: ["Sports", "Cooking"],
+};
+var favouriteActivities; //tells TypeScript to store and array of strings
+favouriteActivities = ["Sports", 1]; //would not work with declaration of string[]
+//using Type any -- lets any value go into an array, but you lose out on the benefits of TypeScript
+console.log(person.name);
+for (var _i = 0, _a = person.hobbies; _i < _a.length; _i++) {
+    var hobby = _a[_i];
+    console.log(hobby);
 }
-
-const number1 = 5;
-const number2 = 2.8;
-
-const result = add(number1, number2);
-console.log(result);
-
-
-function add(n1, n2) {
-  return n1 + n2;
-}
-
-const number1 = "5";
-const number2 = 2.8;
-
-const result = add(number1, number2);
-console.log(result);
-
-breaks the result, JavaScript recieves a string,
-so converts number2 to a string as well, and
-concatenates and returns string (because it recieved 1 string);
-
-
-function add(n1: number, n2: number) {
-  //adding type assignments ensures that function returns and expected value
-  if (typeof n1 !== "number" && typeof n2 !== "number") {
-    throw new Error("Incorrect Input");
-  }
-  return n1 + n2;
-}
-
-const number1 = "5"; //would not compile if 5 was a string
-const number2 = 2.8;
-
-const result = add(number1, number2);
-console.log(result);
-
-Down side f using a forced validation is that you can only
-  find the bug during runtime--typescript will throw a
-  compilation warning
-
-  JavaScript is dynamically typed, meaning you can set a
-  varialbe to a number initially, but later on change
-  that variable to a string and be perfectly fine --
-  why there is a typeof operator if code depends on a certain
-  type.
-
-  TypeScript is statically typed, variables are declared
-  hands on during development. This is to ensure consistency,
-  and making sure that you are clear in what type of type data
-  should be.
-*/
-function add(n1, n2, showResult, phrase) {
-    //added a boolean type
-    // if (typeof n1 !== "number" && typeof n2 !== "number") {
-    //   throw new Error("Incorrect Input");
-    // }
-    var result = n1 + n2;
-    if (showResult) {
-        console.log(phrase + result);
-    }
-    else {
-        return result;
-    }
-}
-var number1 = 5;
-var number2 = 2.8;
-var printResult = true;
-var resultPhrase = "Result is";
-add(number1, number2, printResult, resultPhrase);
