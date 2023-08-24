8b8b784644103ebe26fc74afb91ea935ff8c975d
Daniel <danielseniv@outlook.com>
2023-08-24 17:54:27 -0400

    Explore how to compile entire TypeScript projects/Multiple Files

--- course_starting_project/app.js
+++ course_starting_project/app.js
@@ -1,9 +1,11 @@
-var userInput; //more restrictive than type 'any', makes TypeScript check the currently stored type before assigning to different type
-var userName;
+"use strict";
+let userInput; //more restrictive than type 'any', makes TypeScript check the currently stored type before assigning to different type
+let userName;
 userInput = 5;
 userInput = "Max";
 userName = userInput;
 function generateError(message, code) {
-    throw { message: message, errorCode: code };
+    //explicitly declare that this function should NEVER return anything -- good to let others know your intentions
+    throw { message: message, errorCode: code }; //function never returns anything -- basically crashes the script
 }
 generateError("An error occured!", 500);
