0c34f19834e3ed33a0e90cb52f90ab71e57f0bd2
Daniel <danielseniv@outlook.com>
2023-08-26 13:40:02 -0400

    Explore Code Quality Options in TypeScript config json

--- course_starting_project/src/app.ts
+++ course_starting_project/src/app.ts
@@ -1,5 +1,21 @@
-const button = document.querySelector("button")!;
+let appId = "abc";
 
-button.addEventListener("click", () => {
-  console.log("Clicked");
-});
+const button = document.querySelector("button")!; //'!' syntax tells TypeScript that you know that the button element will not be null
+
+function add(n1: number, n2: number) {
+  if (n1 + n2 > 0) {
+    return n1 + n2;
+  }
+}
+// button.addEventListener("click", () => {
+//   console.log("Clicked");
+// });
+
+function clickHandler(message: string) {
+  let userName = "Max";
+  console.log("Clicked! " + message);
+}
+
+if (button) {
+  button.addEventListener("click", clickHandler.bind(null, "You're welcome!"));
+}
