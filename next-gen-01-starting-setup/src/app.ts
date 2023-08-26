d9d16c0f5974933e0db3df029a604ef8d3e65a44
Daniel <danielseniv@outlook.com>
2023-08-26 14:23:10 -0400

    Understand difference between const, let, var -- block scoped variables

--- next-gen-01-starting-setup/src/app.ts
+++ next-gen-01-starting-setup/src/app.ts
@@ -1,1 +1,20 @@
-// Code goes here!
+const userName = "Max";
+// userName = "Maximilian";
+
+let age = 30; //different from var, as where the variable is available in scope
+
+age = 29;
+
+let result;
+
+function add(a: number, b: number) {
+  let result;
+  result = a + b;
+  return result;
+}
+
+// if (age > 20) {
+//   let isOld = true; //scoped within the curly braces
+// }
+
+// console.log(isOld);
