802a38543c42ba41f9bfdd776080d0134ee9624c
Daniel <danielseniv@outlook.com>
2023-08-26 14:27:31 -0400

    Understand arrow function syntax and take advantage of short, concise code

--- next-gen-01-starting-setup/src/app.ts
+++ next-gen-01-starting-setup/src/app.ts
@@ -7,14 +7,27 @@ age = 29;
 
 let result;
 
-function add(a: number, b: number) {
-  let result;
-  result = a + b;
-  return result;
-}
+// function add(a: number, b: number) {
+//   let result;
+//   result = a + b;
+//   return result;
+// }
 
 // if (age > 20) {
 //   let isOld = true; //scoped within the curly braces
 // }
 
 // console.log(isOld);
+
+const add = (a: number, b: number) => a + b; //if there is one expression, there is an implicit return, dont need curly braces
+console.log(add(2, 5));
+
+const printOutput = (output: string | number) => console.log(output);
+
+const button = document.querySelector("button");
+
+if (button) {
+  button.addEventListener("click", (event) => console.log(event)); //take advantage of short and concise arrow function syntax
+}
+
+printOutput(add(5, 2));
