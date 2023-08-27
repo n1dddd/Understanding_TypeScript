600e91c1bcb089d797fbce9e3eaf3cdb5de4ed3d
Daniel <danielseniv@outlook.com>
2023-08-27 13:22:21 -0400

    Understand Public and Private TypeScript modifiers that enforce more consistent and readable code

--- cl-interfaces-01-starting-setup/src/app.ts
+++ cl-interfaces-01-starting-setup/src/app.ts
@@ -1,6 +1,6 @@
 class Department {
   name: string; //this is a field of a class - defines the key in the object
-
+  private employees: string[] = []; //accessible only to the method acting on the class -- cant access it by explicitly setting a value to the class
   constructor(n: string) {
     //reserved keyword - function tied to the class or object created by the class
     this.name = n;
@@ -10,11 +10,26 @@ class Department {
     //when describe is executed, 'this' should always refer to an instance that is based on the department class
     console.log("Department: " + this.name);
   }
+
+  addEmployee(employee: string) {
+    this.employees.push(employee);
+  }
+
+  printEmployeeInformation() {
+    console.log(this.employees.length);
+    console.log(this.employees);
+  }
 }
 
 const accounting = new Department("Accounting");
+accounting.addEmployee("Max");
+accounting.addEmployee("Daniel");
 
-const accountingCopy = { name: "s", describe: accounting.describe };
+// accounting.employees[2] = "Anna";
+
+// const accountingCopy = { name: "s", describe: accounting.describe };
 
 accounting.describe();
-accountingCopy.describe(); //error here, because describe is being called on a copy of the accounting copy -- which has no name class, so this doesnt work
+accounting.name = "NEW NAME";
+accounting.printEmployeeInformation();
+// accountingCopy.describe(); //error here, because describe is being called on a copy of the accounting copy -- which has no name class, so this doesnt work
