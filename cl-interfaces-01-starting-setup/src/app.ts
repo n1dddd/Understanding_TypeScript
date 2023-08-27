efc2c45be2aa1957ee7c61e8b7c612228dc6c7ec
Daniel <danielseniv@outlook.com>
2023-08-27 14:10:23 -0400

    Explore class inheritance from which some overlapping logic is shared with a parent class to a child

--- cl-interfaces-01-starting-setup/src/app.ts
+++ cl-interfaces-01-starting-setup/src/app.ts
@@ -2,8 +2,9 @@ class Department {
   //   private name: string; //this is a field of a class - defines the key in the object
   private employees: string[] = []; //accessible only to the method acting on the class -- cant access it by explicitly setting a value to the class
 
-  constructor(private id: string, public name: string) {
+  constructor(private readonly id: string, public name: string) {
     //reserved keyword - function tied to the class or object created by the class
+    //'readonly' was added by TypeScript -- value initialized once and cannot be written to
     this.name = name;
   }
 
@@ -22,15 +23,45 @@ class Department {
   }
 }
 
-const accounting = new Department("d1", "Accounting");
-accounting.addEmployee("Max");
-accounting.addEmployee("Daniel");
+class ITDepartment extends Department {
+  constructor(id: string, public admins: string[]) {
+    super(id, "IT"); //has to be called before using 'this' to assign values to the class
+    this.admins = admins;
+  }
+}
 
-// accounting.employees[2] = "Anna";
+class AccountingDepartment extends Department {
+  constructor(id: string, private reports: string[]) {
+    super(id, "Accounting");
+  }
 
-// const accountingCopy = { name: "s", describe: accounting.describe };
+  addReport(text: string) {
+    this.reports.push(text);
+  }
 
-accounting.describe();
-accounting.name = "NEW NAME";
-accounting.printEmployeeInformation();
-// accountingCopy.describe(); //error here, because describe is being called on a copy of the accounting copy -- which has no name class, so this doesnt work
+  printReports() {
+    console.log(this.reports);
+  }
+}
+
+const it = new ITDepartment("d1", ["Max"]);
+it.addEmployee("Max");
+it.addEmployee("Daniel");
+
+// it.employees[2] = "Anna";
+
+// const accountingCopy = { name: "s", describe: it.describe };
+
+it.describe();
+it.name = "NEW NAME";
+it.printEmployeeInformation();
+
+console.log(it);
+
+const accounting = new AccountingDepartment("d2", []);
+
+accounting.addReport("Something went wrong");
+
+accounting.printReports();
+
+// accountingCopy.describe(); //error here, because describe is being called on a copy of the it copy -- which has no name class, so this doesnt work
